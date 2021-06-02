532a33527f0216656fa0cf94f438231aecc15d54
Neal Huang <huangtzuchao@gmail.com>
2021-06-02 10:42:29 -0400

    Added style sheet

--- resources/views/horoscope/index.blade.php
+++ resources/views/horoscope/index.blade.php
@@ -2,7 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
@@ -39,5 +40,6 @@
         </tr>
         @endforeach
     </table>
+
 </body>
 </html>
